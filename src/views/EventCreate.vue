2c21edfc58bcd427e9b50e6618a850d78eb8e5d7
ahamilto99 <ajhamilton15@hotmail.com>
2022-02-09 21:56:36 -0500

    Complete lesson 3 - updating state

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -45,6 +45,7 @@
 
 <script>
 import { v4 as uuidv4 } from 'uuid'
+import EventService from '@/services/EventService.js'
 
 export default {
   data() {
@@ -71,10 +72,18 @@ export default {
     }
   },
   methods: {
-    onSubmit() {
-      this.event.id = uuidv4()
-      this.event.organizer = this.$store.state.user
-      console.log('Event:', this.event)
+    async onSubmit() {
+      const event = {
+        ...this.event, // spread operator
+        id: uuidv4(),
+        organizer: this.$store.state.user
+      }
+      try {
+        await EventService.postEvent(event)
+        this.$store.commit('ADD_EVENT', event)
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 }
